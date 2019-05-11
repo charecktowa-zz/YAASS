a71e8cbfa3309375a69e143376eb40734cc5f42e
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-10 21:19:45 -0500

    Added stdint header to comply with the standard

--- src/fingerprint.h
+++ src/fingerprint.h
@@ -1,13 +1,13 @@
-/*************************************************** 
+/***************************************************
   This is an example sketch for our optical Fingerprint sensor
   Designed specifically to work with the Adafruit Fingerprint sensor
   ----> http://www.adafruit.com/products/751
-  These displays use TTL Serial to communicate, 2 pins are required to 
+  These displays use TTL Serial to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -16,6 +16,7 @@
 #define _fingerprint_h
 
 #include <Arduino.h>
+#include <stdint.h>
 
 #include <Adafruit_Fingerprint.h>
 
